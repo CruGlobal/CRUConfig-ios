5b26aa7e8b7b78e5e84a3f9a67fa92363565fdbc
Harro <michael.harrison@cru.org>
2017-05-09 09:57:45 -0400

    adds implementation for snake case to camel case

--- CRUConfig/Classes/NSString+CaseTransformations.h
+++ CRUConfig/Classes/NSString+CaseTransformations.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-@interface NSString (t)
+@interface NSString (CaseTransformations)
 
 - (NSString *)snakeCaseToCamelCase;
 
--- CRUConfig/Classes/NSString+CaseTransformations.m
+++ CRUConfig/Classes/NSString+CaseTransformations.m
@@ -13,11 +13,25 @@
 - (NSString *)snakeCaseToCamelCase {
     
     NSArray<NSString *> *components = [self componentsSeparatedByString:@"_"];
+    NSMutableArray<NSString *> *camelCaseComponents = @[].mutableCopy;
+    
     if (components.count <= 1) {
         return self;
     }
     
-    return self;
+    [components enumerateObjectsUsingBlock:^(NSString * _Nonnull component, NSUInteger index, BOOL * _Nonnull stop) {
+        if (component.capitalizedString) {
+            [camelCaseComponents addObject:component.capitalizedString];
+        }
+    }];
+    
+    if (camelCaseComponents.count > 0 && components[0].lowercaseString) {
+        camelCaseComponents[0] = components[0].lowercaseString;
+    } else if (camelCaseComponents.count > 0) {
+        camelCaseComponents[0] = components[0];
+    }
+    
+    return [camelCaseComponents componentsJoinedByString:@""];
 }
     
 @end
