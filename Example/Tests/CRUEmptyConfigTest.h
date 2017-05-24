e091ea372b37a0f462e17b0ec4182c668c522039
Harro <michael.harrison@cru.org>
2017-05-24 16:25:20 -0400

    adds tests for matching keys from plist files to properties on a CRUEmptyConfig subclass

---
+++ Example/Tests/CRUEmptyConfigTest.h
@@ -0,0 +1,22 @@
+//
+//  CRUEmptyConfigTest.h
+//  CRUConfig
+//
+//  Created by Michael Harrison on 5/24/17.
+//  Copyright © 2017 Harro. All rights reserved.
+//
+
+#import <CRUConfig/CRUConfig.h>
+
+@interface CRUEmptyConfigTest : CRUEmptyConfig
+
+@property (nonatomic, strong) NSArray *numberArray;
+@property (nonatomic, strong) NSDictionary *directionDictionary;
+@property (nonatomic, strong) NSNumber *myBoolean;
+@property (nonatomic, strong) NSData *DATAINTHEHOUSE;
+@property (nonatomic, strong) NSDate *date;
+@property (nonatomic, strong) NSNumber *medium_number;
+@property (nonatomic, strong) NSString *myAmazingString;
+@property (nonatomic, strong) NSString *my_amazing_string;
+
+@end
---
+++ Example/Tests/CRUEmptyConfigTest.m
@@ -0,0 +1,13 @@
+//
+//  CRUEmptyConfigTest.m
+//  CRUConfig
+//
+//  Created by Michael Harrison on 5/24/17.
+//  Copyright © 2017 Harro. All rights reserved.
+//
+
+#import "CRUEmptyConfigTest.h"
+
+@implementation CRUEmptyConfigTest
+
+@end
